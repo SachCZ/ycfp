d271401aa02a2351d81b0a527fe50f6c5e3df28c
Martin <martin.sachin@gmail.com>
2021-04-12 19:37:07 +0200

    Added nested validation.

--- include/ycfp.h
+++ include/ycfp.h
@@ -3,20 +3,18 @@
 
 #include <iostream>
 #include <yaml-cpp/yaml.h>
+#include <any>
+#include <utility>
 
 namespace ycfp {
     struct Object {
     };
 
-    struct ExpectationType {
+    struct NodeType {
     };
 
     struct ValidationError {
         std::string message;
-    };
-
-    struct ValidationWarning {
-
     };
 
     enum class Existence {
@@ -25,56 +23,209 @@ namespace ycfp {
     };
 
     template<typename T = Object>
-    class Expectation : ExpectationType {
+    class Node : NodeType {
     public:
-        Expectation(std::string_view key, Existence existence): key(key), existence(existence) {}
+        Node(std::string_view key, Existence existence) : key(key), existence(existence) {}
 
         std::optional<T> parse(const YAML::Node &node) const {
-            if (!node[key]) return {};
-            return node[key].as<T>();
+            if (!node) return {};
+            return node.as<T>();
         }
 
-        std::vector<ValidationError> validate(const std::optional<T>& value) const {
+        [[nodiscard]] std::vector<ValidationError> validate(const std::any &value) const {
             std::vector<ValidationError> errors;
-            if (existence == Existence::Required && !value) {
-                errors.emplace_back(ValidationError{key + " is required!"});
+            try {
+                auto extractedValue = std::any_cast<std::optional<T>>(value);
+                if (existence == Existence::Required && !extractedValue) {
+                    errors.emplace_back(ValidationError{key + " is required!"});
+                }
+            } catch (const std::bad_any_cast &) {
+                if (existence == Existence::Required) {
+                    errors.emplace_back(ValidationError{key + " is required!"});
+                }
             }
             return errors;
         }
 
+        std::string key;
+        static constexpr bool isMap = false;
     private:
         Existence existence;
-        std::string key;
     };
 
-    template<>
-    class Expectation<Object> : ExpectationType {
+
+    class AccessError : public std::exception {
     public:
-        template<typename... Ts>
-        explicit Expectation(const Ts &... expectations) {
-            assertExpectTypes(expectations...);
+        explicit AccessError(std::string_view message) noexcept: message(message) {}
+
+        ~AccessError() override = default;
+
+        [[nodiscard]] const char *what() const noexcept override {
+            return this->message.c_str();
         }
 
     private:
-        static void assertExpectTypes() {}
+        std::string message;
+    };
 
-        template<typename T, typename... Ts>
-        static void assertExpectTypes(const T &expectation, const Ts &... rest) {
-            static_assert(std::is_base_of<ExpectationType, T>::value, "Not an ExpectationType");
-            assertExpectTypes(rest...);
+    class ParsingError : public std::exception {
+    public:
+        explicit ParsingError(std::string_view message) noexcept: message(message) {}
+
+        ~ParsingError() override = default;
+
+        [[nodiscard]] const char *what() const noexcept override {
+            return this->message.c_str();
         }
 
+    private:
+        std::string message;
+    };
+
+    template<typename Res, typename Exp>
+    class Result {
+    public:
+        explicit Result(const Res &res, const Exp &exp) : res(res), exp(exp) {}
+
+        template<typename T>
+        T get(std::initializer_list<std::string> keys) {
+            if (keys.size() == 0)
+                throw AccessError("At least one key must be supplied!");
+            if constexpr (Exp::isMap) {
+                if (res) {
+                    try {
+                        auto currentMap = res.value();
+                        std::for_each(std::begin(keys), std::prev(std::end(keys)), [&currentMap](const auto &key) {
+                            currentMap = std::any_cast<std::optional<std::map<std::string, std::any>>>(
+                                    currentMap[key]).value();
+                        });
+                        return std::any_cast<std::optional<T>>(currentMap[*std::prev(std::end(keys))]).value();
+                    } catch (const std::bad_any_cast&) {
+                        throw AccessError("Accessing unparsed value!");
+                    } catch (const std::bad_optional_access&) {
+                        throw AccessError("Accessing unparsed value!");
+                    }
+                } else {
+                    throw AccessError("Accessing unparsed value!");
+                }
+            } else {
+                if (keys.size() == 1) {
+                    if (res && *keys.begin() == exp.key) {
+                        return res.value();
+                    } else {
+                        throw AccessError("Accessing unparsed value!");
+                    }
+                } else {
+                    throw AccessError("Multiple keys for for single value expectation!");
+                }
+            }
+        }
+
+    private:
+        Res res;
+        Exp exp;
     };
 
     template<typename T, typename Log = std::vector<ValidationError>>
-    auto parseExpected(const YAML::Node &parent, const T &expectation, Log* log = nullptr) {
-        auto value = expectation.parse(parent);
+    auto parseExpected(const YAML::Node &node, const T &expectation, Log *log = nullptr) {
+        auto value = expectation.parse(node);
         if (log) {
-            const auto& errors = expectation.validate(value);
+            const auto errors = expectation.validate(value);
             log->insert(std::end(*log), std::begin(errors), std::end(errors));
         }
-        return value;
+        return Result(value, expectation);
     }
+
+    template<>
+    class Node<Object> : NodeType {
+    public:
+        template<typename T, typename... Ts>
+        explicit Node(const T &first, const Ts &... expectations) {
+
+            constexpr bool isRoot = !std::is_convertible<T, std::string>::value;
+            if constexpr (!isRoot) {
+                key = first;
+            }
+
+            parseCallback = [this, &first, expectations...](const YAML::Node &node) {
+                std::map<std::string, std::any> result;
+                if constexpr (isRoot) {
+                    parseRecursively(result, node, first, expectations...);
+                } else {
+                    parseRecursively(result, node, expectations...);
+                }
+                return result;
+            };
+
+            validateCallback = [expectations..., &first](const std::any &value, std::vector<ValidationError>& errors) {
+                if constexpr (isRoot) {
+                    validateRecursively(errors, value, first, expectations...);
+                } else {
+
+                    validateRecursively(errors, value, expectations...);
+                }
+                return errors;
+            };
+        }
+
+        [[nodiscard]] std::optional<std::map<std::string, std::any>> parse(const YAML::Node &node) const {
+            if (!node) return {};
+            return parseCallback(node);
+        }
+
+        [[nodiscard]] std::vector<ValidationError>
+        validate(const std::any &value) const {
+            auto castedValue = std::any_cast<std::optional<std::map<std::string, std::any>>>(value);
+            std::vector<ValidationError> errors{0};
+            if (!castedValue) {
+                errors.emplace_back(ValidationError{this->key + " is required!"});
+                return errors;
+            }
+            return validateCallback(value, errors);
+        }
+
+        static constexpr bool isMap = true;
+        std::string key;
+    private:
+        std::function<std::map<std::string, std::any>(YAML::Node)> parseCallback;
+
+        std::function<std::vector<ValidationError>(std::any value, std::vector<ValidationError>& errors)> validateCallback;
+
+        static void assertExpectTypes() {}
+
+        template<typename Node, typename... Rest>
+        static void assertExpectTypes(const Node &expectation, const Rest &... rest) {
+            static_assert(std::is_base_of<NodeType, Node>::value, "Not an NodeType");
+            assertExpectTypes(rest...);
+        }
+
+        static void parseRecursively(std::map<std::string, std::any> &, const YAML::Node &) {}
+
+        template<typename Node, typename... Rest>
+        static void parseRecursively(std::map<std::string, std::any> &result, const YAML::Node &node,
+                                     const Node &expectation, const Rest &... rest) {
+            if (expectation.key.empty()) {
+                throw ParsingError("Root must be a parent expectation");
+            }
+            result[expectation.key] = expectation.parse(node[expectation.key]);
+            parseRecursively(result, node, rest...);
+        }
+
+        static void validateRecursively(std::vector<ValidationError> &, const std::any &) {}
+
+        template<typename Node, typename... Rest>
+        static void validateRecursively(
+                std::vector<ValidationError> &errors,
+                const std::any &value,
+                const Node &expectation,
+                const Rest &... rest
+        ) {
+            auto castedValue = std::any_cast<std::optional<std::map<std::string, std::any>>>(value).value();
+            const auto &result = expectation.validate(castedValue[expectation.key]);
+            errors.insert(std::begin(errors), std::begin(result), std::end(result));
+            validateRecursively(errors, value, rest...);
+        }
+    };
 }
 
 #endif //YCFP_YCFP_H
--- test/unit/expectation_test.cpp
+++ test/unit/expectation_test.cpp
@@ -8,15 +8,94 @@ using ex = ycfp::Existence;
 TEST_CASE("A value expectation can be parsed to the value", "[expectation]") {
     YAML::Node node;
     node["name"] = "Jack";
-    Expectation<string> expectation("name", ex::Required);
-    REQUIRE(parseExpected(node, expectation) == "Jack");
+    Node<string> expectation("name", ex::Required);
+    auto result = parseExpected(node["name"], expectation);
+    REQUIRE(result.get<string>({"name"}) == "Jack");
 }
 
 TEST_CASE("Required value not parsed or parsed as null is an error", "[expectation]") {
     YAML::Node node;
-    Expectation<string> expectation("name", ex::Required);
-    vector<ValidationError> log;
-    parseExpected(node, expectation, &log);
+    Node<string> expectation("name", ex::Required);
+    vector<ValidationError> log{0};
+    auto result = parseExpected(node["name"], expectation, &log);
     REQUIRE(log.size() == 1);
     REQUIRE(log[0].message == "name is required!");
 }
+
+TEST_CASE("Accessing expected value that was not successfully parsed is an error", "[expectation]") {
+    YAML::Node node;
+    Node<string> expectation("name", ex::Required);
+    auto result = parseExpected(node["name"], expectation);
+    REQUIRE_THROWS_AS(result.get<string>({"name"}), AccessError);
+}
+
+
+TEST_CASE("Accessing expected value with wrong key is an error", "[expectation]") {
+    YAML::Node node;
+    node["name"] = "Jack";
+    Node<string> expectation("name", ex::Required);
+    auto result = parseExpected(node["name"], expectation);
+    REQUIRE_THROWS_AS(result.get<string>({"not_name"}), AccessError);
+}
+
+TEST_CASE("Object expectation parses all expected values and returns correct result", "[expectation]") {
+    YAML::Node node;
+    node["name"] = "Jack";
+    node["age"] = (unsigned int) 33;
+    Node<> expectation(
+            Node<string>{"name", ex::Required},
+            Node<unsigned int>{"age", ex::Required}
+    );
+    auto result = parseExpected(node, expectation);
+    CHECK(result.get<string>({"name"}) == "Jack");
+    REQUIRE(result.get<unsigned int>({"age"}) == 33);
+}
+
+
+TEST_CASE("Nested object expectations parse values can be retrieved", "[expectation]") {
+    YAML::Node node;
+    node["data"]["name"] = "Jack";
+    node["data"]["age"] = (unsigned int) 33;
+    Node<> expectation(
+            "root",
+            Node<>{
+                    "data",
+                    Node<string>{"name", ex::Required},
+                    Node<unsigned int>{"age", ex::Required}
+            }
+    );
+    auto result = parseExpected(node, expectation);
+    CHECK(result.get<string>({"data", "name"}) == "Jack");
+    REQUIRE(result.get<unsigned int>({"data", "age"}) == 33);
+}
+
+TEST_CASE("Accessing invalid nested object throws", "[expectation]") {
+    YAML::Node node;
+    auto result = parseExpected(node, Node<>{"node"});
+    REQUIRE_THROWS_AS(result.get<string>({"person", "data", "name"}), AccessError);
+}
+
+TEST_CASE("Accessing valid but not parsed expectation logs and throws", "[expectation]") {
+    YAML::Node node;
+    std::vector<ValidationError> log{0};
+    Node<> expectation{
+            "root",
+            Node<>{
+                    "data",
+                    Node<string>{"name", ex::Required}
+            }
+    };
+    auto result = parseExpected(node, expectation, &log);
+    REQUIRE(log.size() == 1);
+    REQUIRE(log[0].message == "data is required!");
+    REQUIRE_THROWS_AS(result.get<string>({"data", "name"}), AccessError);
+}
+
+TEST_CASE("Parsing expectation structure that has root as child is an error") {
+    YAML::Node node;
+    REQUIRE_THROWS_AS(parseExpected(node, Node<>{
+            "root",
+            Node<>{Node<std::string>{"name", ex::Required}}
+    }), ParsingError);
+}
+
